2e68554e8a749a9c7995e4fcdbefd9c8c4f5e2d9
wegaputra <80662754+wegaputra@users.noreply.github.com>
2021-04-09 10:27:04 +0700

    Update ComputerBuyerPredictor.java

--- src/predictor/ComputerBuyerPredictor.java
+++ src/predictor/ComputerBuyerPredictor.java
@@ -13,6 +13,6 @@ public class ComputerBuyerPredictor {
         } else{
             potensiMenjadiCustomer = false;
         }
-    return potensiMenjadiCustomer;
+        return potensiMenjadiCustomer;
     }
 }
